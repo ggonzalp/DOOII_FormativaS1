c53e22496e3a55d25528cb963449f624b206a070
ggonzalp <gabr.gonzalezp@duocuc.cl>
2026-08-21 19:05:13 -0600

    PedidoEncomienda

--- PedidoComida.java
+++ PedidoComida.java
@@ -8,52 +8,56 @@ public class PedidoComida extends Pedido {
 
     /**
      * Constructor de la clase PedidoComida.
-     * @param tipoPedido Tipo de pedido.
-     * @param idPedido Número de identificación del pedido.
+     *
+     * @param tipoPedido       Tipo de pedido.
+     * @param idPedido         Número de identificación del pedido.
      * @param direccionEntrega Dirección de entrega del pedido.
-     * @param validarMochila Validación de mochila térmica.
+     * @param distanciaKm      Distancia en kilómetros del lugar de entrega del producto.
+     * @param validarMochila   Valida la presencia de mochila térmica.
      */
-    public PedidoComida(String tipoPedido, int idPedido, DireccionEntrega direccionEntrega, boolean validarMochila) {
-        super(tipoPedido, idPedido, direccionEntrega);
+    public PedidoComida(String tipoPedido, int idPedido, DireccionEntrega direccionEntrega, int distanciaKm, boolean validarMochila) {
+        super(tipoPedido, idPedido, direccionEntrega, distanciaKm);
 
         this.validarMochila = validarMochila;
     }
-    //Método getter
-     public boolean getValidarMochila() {
+
+    //Metodo getter
+    public boolean getValidarMochila() {
         return validarMochila;
     }
-    //Método setter
+
+    //Metodo setter
     public void setValidarMochila(boolean validarMochila) {
         this.validarMochila = validarMochila;
     }
 
     /**
-     * Método asignarRepartidor
-     * muestra la información en consola.
-     */
-    @Override
-    public void asignarRepartidor() {
-        super.asignarRepartidor();
-        System.out.println("\nValidando mochila térmica...");
-    }
-
-    /**
-     * Método asignarRepartidor
+     * Metodo asignarRepartidor
      * @param nombreRepartidor Nombre del repartidor.
      * @throws IllegalStateException Si el repartidor no cuenta con mochila térmica no es posible asignar el pedido.
      */
     @Override
     public void asignarRepartidor(String nombreRepartidor) {
-        asignarRepartidor();
+        super.asignarRepartidor();
+        System.out.println("\nValidando mochila térmica...");
 
         //Validador de validarMochila.
         if (validarMochila == true) {
             System.out.println("Mochila térmica: Sí.");
-            System.out.println("\nMochila térmica validada correctamente.");
+            System.out.println("Mochila térmica validada correctamente.");
         } else {
             throw new IllegalStateException("Mochila térmica: No.");
         }
-        System.out.println("Asignando repartidor...");
+        System.out.println("\nAsignando repartidor...");
         System.out.println("Repartidor asignado: " + nombreRepartidor);
+    }
+
+    /**
+     * Metodo que calcula el tiempo de entrega del pedido.
+     * @return tiempo estimado de entrega del pedido.
+     */
+    @Override
+    public int calcularTiempoEntrega() {
+        return 15 + (2 * getDistanciaKm());
     }
 }
--- PedidoEncomienda.java
+++ PedidoEncomienda.java
@@ -8,55 +8,54 @@ public class PedidoEncomienda extends Pedido {
 
     /**
      * Constructor de la clase PedidoEncomienda
-     *
      * @param tipoPedido         Tipo de pedido.
      * @param idPedido           Número de identificación del pedido.
      * @param direccionEntrega   Dirección de entrega del pedido.
-     * @param validarEmpaquetado Validación de empaquetado (peso y embalaje).
+     * @param distanciaKm        Distancia en kilómetros del lugar de entrega del producto.
+     * @param validarEmpaquetado Validar el peso y empaquetado del pedido.
      */
-    public PedidoEncomienda(String tipoPedido, int idPedido, DireccionEntrega direccionEntrega, boolean validarEmpaquetado) {
-        super(tipoPedido, idPedido, direccionEntrega);
+    public PedidoEncomienda(String tipoPedido, int idPedido, DireccionEntrega direccionEntrega, int distanciaKm, boolean validarEmpaquetado) {
+        super(tipoPedido, idPedido, direccionEntrega, distanciaKm);
 
         this.validarEmpaquetado = validarEmpaquetado;
     }
 
-    //Método getter
-    public boolean getValidarEmpaquetado() {
+    public boolean isValidarEmpaquetado() {
         return validarEmpaquetado;
     }
 
-    //Método setter
     public void setValidarEmpaquetado(boolean validarEmpaquetado) {
         this.validarEmpaquetado = validarEmpaquetado;
     }
 
     /**
-     * Método asignarRepartidor
-     * muestra la información en consola.
-     */
-    @Override
-    public void asignarRepartidor() {
-        super.asignarRepartidor();
-        System.out.println("\nValidando peso y embalaje... ");
-    }
-
-    /**
-     * Método asignarRepartidor
+     * Metodo asignarRepartidor
      * @param nombreRepartidor Nombre del repartidor.
      * @throws IllegalStateException Si el paquete no cumple con el peso ni embalaje adecuado, no es posible asignar el pedido.
      */
     @Override
     public void asignarRepartidor(String nombreRepartidor) {
-        asignarRepartidor();
+        super.asignarRepartidor();
+        System.out.println("\nValidando peso y embalaje... ");
 
         //Validador de validarEmpaquetado.
         if (validarEmpaquetado == true) {
             System.out.println("Peso y empaquetado correctos: Sí.");
-            System.out.println("\nPeso y empaquetado validados correctamente.");
+            System.out.println("Peso y empaquetado validados correctamente.");
         } else {
             throw new IllegalStateException("Peso y empaquetado correctos: No.");
         }
-        System.out.println("Asignando repartidor...");
+        System.out.println("\nAsignando repartidor...");
         System.out.println("Repartidor asignado: " + nombreRepartidor);
+    }
+
+    /**
+     * Metodo que calcula el tiempo de entrega del pedido.
+     * @return tiempo estimado de entrega del pedido.
+     */
+
+    @Override
+    public int calcularTiempoEntrega() {
+       return ((int) Math.round(20 + (1.5 * getDistanciaKm())));
     }
 }
