48cc20f571c2da1f2b3bfd528f0810129bd95e69
ggonzalp <gabr.gonzalezp@duocuc.cl>
2026-08-21 19:09:49 -0600

    PedidoExpress

--- PedidoExpress.java
+++ PedidoExpress.java
@@ -11,55 +11,56 @@ public class PedidoExpress extends Pedido {
      * @param tipoPedido Tipo de pedido.
      * @param idPedido Número de identificación del pedido.
      * @param direccionEntrega Dirección de entrega del pedido.
-     * @param validarRepartidorCercano validación de ubicación cercana.
+     * @param distanciaKm Distancia en kilómetros del lugar de entrega del producto.
+     * @param validarRepartidorCercano Valida la cercanía del repartidor al punto de entrega.
      */
 
-    public PedidoExpress(String tipoPedido, int idPedido, DireccionEntrega direccionEntrega, boolean validarRepartidorCercano) {
-        super(tipoPedido, idPedido, direccionEntrega);
+    public PedidoExpress(String tipoPedido, int idPedido, DireccionEntrega direccionEntrega, int distanciaKm, boolean validarRepartidorCercano) {
+        super(tipoPedido, idPedido, direccionEntrega, distanciaKm);
 
         this.validarRepartidorCercano = validarRepartidorCercano;
     }
 
-    //Método getter
-    public boolean getValidarRepartidorCercano() {
+    public boolean isValidarRepartidorCercano() {
         return validarRepartidorCercano;
     }
 
-    //Método setter
     public void setValidarRepartidorCercano(boolean validarRepartidorCercano) {
         this.validarRepartidorCercano = validarRepartidorCercano;
     }
 
     /**
-     * Método asignarRepartidor
-     * muestra la información en consola.
-     */
-
-    @Override
-    public void asignarRepartidor() {
-        super.asignarRepartidor();
-        System.out.println("\nValidando cercanía del repartidor...");
-    }
-    /**
-     * Método asignarRepartidor
-     *
+     * Metodo asignarRepartidor
      * @param nombreRepartidor Nombre del repartidor.
      * @throws IllegalStateException Si el repartidor está lejos de la ubicación de la solicitud de reparto, no es posible asignar el pedido.
      */
-
     @Override
     public void asignarRepartidor(String nombreRepartidor) {
-        asignarRepartidor();
+        super.asignarRepartidor();
+        System.out.println("\nValidando cercanía del repartidor... ");
 
         //Validador de validarRepartidorCercano.
         if (validarRepartidorCercano == true) {
             System.out.println("Repartidor cercano: Sí.");
-            System.out.println("\nCercanía validada con GPS correctamente.");
+            System.out.println("Cercanía validada con GPS correctamente.");
         } else {
             throw new IllegalStateException("Repartidor cercano: No.");
         }
-        System.out.println("Asignando repartidor...");
+        System.out.println("\nAsignando repartidor...");
         System.out.println("Repartidor asignado: " + nombreRepartidor);
+    }
+
+    /**
+     * Metodo que calcula el tiempo de entrega del pedido
+     * @return tiempo estimado de entrega del pedido.
+     */
+    @Override
+    public int calcularTiempoEntrega() {
+        int tiempo = 10;
+        if (getDistanciaKm() > 5) {
+            tiempo = tiempo + 5;
+        }
+        return tiempo;
     }
 }
 
